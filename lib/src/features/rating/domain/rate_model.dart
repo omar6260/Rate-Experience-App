6ee51f3af4a07b2244005d44abacd64e9711e1e6
Oumar <foumar404@gmail.com>
2024-08-06 11:13:44 +0000

    feat: add Rate Model

---
+++ lib/src/features/rating/domain/rate_model.dart
@@ -0,0 +1,15 @@
+
+
+class RateModel {
+  final String? image;
+  final String? text;
+  final String? label;
+  final String? color;
+
+  RateModel({
+    this.image,
+    this.text,
+    this.label,
+    this.color,
+  });
+}
